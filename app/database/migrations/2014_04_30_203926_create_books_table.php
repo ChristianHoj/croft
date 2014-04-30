6d8d1b5dd227afcb6f8317bd84bd7e7b4a984149
Christian Høj <cbh@ucn.dk>
2014-04-30 23:19:34 +0200

    Add Book model and migration

---
+++ app/database/migrations/2014_04_30_203926_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('books', function($table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->string('description');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('books');
+	}
+
+}
